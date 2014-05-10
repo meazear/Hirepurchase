e33826996ca1024a81f218dc0f72f63c30d27d62
meazear <pauyleng@gmail.com>
2014-05-10 14:13:50 +0700

    Create VehicleConnectivityController.java

---
+++ src/java/com/hirepurchase/web/SecuritiesSystem/Vehicle/VehicleConnectivityController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.hirepurchase.web.SecuritiesSystem.Vehicle;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author FUJITSU
+ */
+@Controller
+public class VehicleConnectivityController {
+    @RequestMapping(value="/carconnectivity",method = RequestMethod.GET)
+	public String displayCarType(ModelMap model) {     
+            System.out.println("IN CarConnectivityController");
+		return "master/CarConnectivity";
+ 
+	}
+}
